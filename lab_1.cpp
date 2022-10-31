c8c79719fc318d8c1ac17a9f615cb2c413b2edac
Merluna21 <115717697+Merluna21@users.noreply.github.com>
2022-10-31 14:10:42 +0200

    Add files via upload

---
+++ lab_1.cpp
@@ -0,0 +1,44 @@
+﻿#include <conio.h>
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+class Square {
+private:
+	double side;
+public:
+
+	Square() : side(0) {}
+	
+	double Counting() {
+		double area = side * side;
+		return area;
+	}
+
+	void Setside() {
+		cout << "Введіть сторону квадрата: " << endl;
+		cin >> side;
+	}
+
+	void print_sqr()
+	{
+		if (side < 0)
+			cout << "\nВи ввели від'ємне значення сторони" << endl;
+		else
+		{
+			cout << "Сторона квадрата: " << side << endl;
+			cout << "Сторона квадрата: " << Counting() << endl << endl;
+		}
+	}
+};
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	Square a;
+	a.Setside();
+	a.Counting();
+	a.print_sqr();
+	return 0;
+}
